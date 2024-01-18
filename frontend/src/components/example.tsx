b4fc91444ec80036b981871628bd5b05990b61d0
Mieres7 <vicente.mieres@usach.cl>
2024-01-18 19:25:22 -0300

    feat: create general structure for frontend proyect

---
+++ frontend/src/components/example.tsx
@@ -0,0 +1,15 @@
+import './src/App.css'
+
+export const Example = () => {
+    return(
+        <div>
+            <h1 className="{styles.aa}">Hola</h1>
+
+            <form action="">
+                <button>Hola</button>
+            </form>
+        </div>
+
+    )
+
+}
